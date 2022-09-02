2d04f997bceb38b425eacb267da569d14fd7606d
leshazimoglyad <102586567+leshazimoglyad@users.noreply.github.com>
2022-09-02 16:23:45 +0300

    аыиав

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+function createMarkup(images) {
+  return images
+    .map((image) => {
+      return `
+<a class="gallery__item href="${image.original}">
+    <img
+    class="gallery__image"
+    src="${image.preview}"
+    alt="${image.description}"
+    />
+</a>
+`;
+    })
+    .join(``);
+}
+const createdMarkup = createMarkup(galleryItems);
+const galleryElement = document.querySelector(`.gallery`);
+galleryElement.innerHTML = createdMarkup;
 
-console.log(galleryItems);
+new SimpleLightbox(".gallery ", {
+  captionsData: "alt",
+  captionDelay: 250,
+  animationSpeed: 250,
+});
