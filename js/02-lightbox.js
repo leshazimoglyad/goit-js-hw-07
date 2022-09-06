57e5a5c632964a3ceb5181027585528a8830406c
leshazimoglyad <102586567+leshazimoglyad@users.noreply.github.com>
2022-09-06 14:29:06 +0300

    вя

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,24 +1,19 @@
 import { galleryItems } from "./gallery-items.js";
 function createMarkup(images) {
   return images
-    .map((image) => {
-      return `
-<a class="gallery__item href="${image.original}">
-    <img
-    class="gallery__image"
-    src="${image.preview}"
-    alt="${image.description}"
-    />
-</a>
-`;
-    })
-    .join(``);
+  .map(
+    ({ preview, original, description }) =>
+      `<li><a class="gallery__item" href=${original}>
+<img class="gallery__image" src=${preview} alt="${description}" />
+</a></li>`,
+  )
+  .join('');
 }
 const createdMarkup = createMarkup(galleryItems);
 const galleryElement = document.querySelector(`.gallery`);
 galleryElement.innerHTML = createdMarkup;
 
-new SimpleLightbox(".gallery ", {
+new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
   animationSpeed: 250,
